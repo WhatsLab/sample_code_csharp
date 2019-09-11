b8dee1e2ed8e4c3764b2ffe5c6508f21fb200920
= <=>
2019-09-11 12:57:09 +0300

    Added Branch Detection

--- Program.cs
+++ Program.cs
@@ -12,47 +12,71 @@ namespace integrationExampleUsingCsharp
 {
   class Program
   {
-    private const string RequestUri = ""; // Endpoint URL (e.g: https://nana.sa/api/sync_store_products_by_key)
-    private const string Token = ""; // JWT Token (Token will be provided for each branch individually)
-    private const string query = ""; // Query String (e.g select unit_price as price, item_number as sku, barcode as barcode from products)
+    private const string RequestUri = ""; // nana enpoint URL e.g (https://nana.sa/api/sync_store_products_by_key)
+    private static string getBranchQuery = ""; // query for branches and corresponding tokens
+    private static string StoreId = ""; // local branch Id
+    private static string Token = ""; // corresponding token
+    private static string getDataQuery = ""; // query records either changed products or daily sales
+
     static void Main(string[] args)
     {
-        string connStr = "server=localhost;database=database;uid=root;pwd=root"; // Connection String
-        using (MySqlConnection conn = new MySqlConnection(connStr))
+        string connStr = "server=localhost;database=dotnet;uid=root;pwd=root"; // connection string to the database
+        using (MySqlConnection conn = new MySqlConnection(connStr)) // initiate connection
         {
             try
             {
-                Console.WriteLine("Connecting To MySql ...");
-                using (MySqlDataAdapter da = new MySqlDataAdapter()) // Generate The MySql Adapter 
+              Console.WriteLine("Connecting To MySql ...");
+              using (MySqlDataAdapter da = new MySqlDataAdapter()) // create new instance for the mysql adapter
+              {
+                using (DataTable dt1 = new DataTable()) // initiate new data table
                 {
-                    using (DataTable dt = new DataTable())
+                  using (MySqlCommand sqlCommand1 = conn.CreateCommand()) // prepare new sql command
+                  {
+                    getBranchQuery = ""; // write query text
+                    // query example (select name, token from stores)
+                    sqlCommand1.CommandType = CommandType.Text; // define command type
+                    sqlCommand1.CommandText = getBranchQuery; // add command text
+                    da.SelectCommand = sqlCommand1; // execute command
+                    da.Fill(dt1); // fill record to the data table declared above
+                    sqlCommand1.Dispose();
+                    da.Dispose();
+                    foreach (DataRow row in dt1.Rows) // foreach branch get either changed records or daily sales
                     {
-                        using (MySqlCommand sqlCommand = conn.CreateCommand()) // Create MySql Command
+                      Token = row["token"].ToString(); // assign store token
+                      StoreId = row["name"].ToString(); // assign store id
+                      using (DataTable dt = new DataTable()) // initiate new data table
+                      {
+                        using (MySqlCommand sqlCommand = conn.CreateCommand()) // prepare new sql command
                         {
-                            sqlCommand.CommandType = CommandType.Text;
-                            sqlCommand.CommandText = query;
-                            da.SelectCommand = sqlCommand; // Execute MySql Command
-                            da.Fill(dt); // Dump Result into DataTable
-                            sqlCommand.Dispose();
-                            da.Dispose();
-                            Console.WriteLine("Preparing Batches");
-                            var tr = dt.Rows.Count; // Get Total Rows Count
-                            tr = (int)Math.Ceiling((Double)tr / 10); // Divide total by the batches
-                            var skipCount = 0;
-                            for (int i = 0; i < tr; i++) // Loop through the total table rows
+                          getDataQuery = ""; // write query text
+                          // select name, phone, store_id from users where store_id='" + StoreId + "'
+                          sqlCommand.CommandType = CommandType.Text; // define command type
+                          sqlCommand.CommandText = getDataQuery; // add command text
+                          da.SelectCommand = sqlCommand; // execute command
+                          da.Fill(dt); // fill record to the data table declared above
+                          sqlCommand.Dispose();
+                          da.Dispose();
+                          Console.WriteLine("Preparing Batches");
+                          var tr = dt.Rows.Count; // get total records
+                          tr = (int)Math.Ceiling((Double)tr / 10); // divide batches 10 per each
+                          var skipCount = 0; // prepare skipping records
+                          for (int i = 0; i < tr; i++) // prepare skipping records
+                          {
+                            Console.WriteLine("Sending " + (i + 1) + " Batch");
+                            var dataBatch = dt.AsEnumerable().Skip(skipCount).Take(10); // take every tr number and skip
+                            using (DataTable copyTableData = dataBatch.CopyToDataTable<DataRow>()) // copy batched to new datatable
                             {
-                                Console.WriteLine("Sending " + (i + 1) + " Batch");
-                                var dataBatch = dt.AsEnumerable().Skip(skipCount).Take(10); // Pick 1000 from the total results 
-                                using (DataTable copyTableData = dataBatch.CopyToDataTable<DataRow>()) // Copy batched to new datatable
-                                {
-                                    var jsonResult = JsonConvert.SerializeObject(new {product_arrays = copyTableData}); // Serialize Copied Data To JSON Object (product_arrays key for product_sync and barcode_arrays for daily sales)
-                                    sendRequest(jsonResult);
-                                    skipCount += 10; // Increase skipCount by 1000 to pick the next batch
-                                }
+                              var jsonResult = JsonConvert.SerializeObject(new {product_arrays = copyTableData}); // serialize copied data to JSON object (product_arrays key for product_sync and barcode_arrays for daily sales)
+                              sendRequest(jsonResult); // send request
+                              skipCount += 10; // skip the next batch
                             }
+                          }
                         }
+                      }
                     }
+                  }
                 }
+              }
             }
             catch (Exception e)
             {
@@ -68,13 +92,13 @@ namespace integrationExampleUsingCsharp
       {
         using (HttpClient client = new HttpClient())
           {
-              client.DefaultRequestHeaders.Add("Authorization", Token); // Add Authorization Header defined on line 16
+              client.DefaultRequestHeaders.Add("Authorization", Token); // append token got from stores query
 
-              using (HttpContent content = new StringContent(data, Encoding.UTF8, "application/json")) // Prepare JSON Payload To Send
+              using (HttpContent content = new StringContent(data, Encoding.UTF8, "application/json")) // prepare data and headers
               {
-                  using (HttpResponseMessage response = client.PostAsync(RequestUri, content).Result) // Send HTTP request and store result in (response)
+                  using (HttpResponseMessage response = client.PostAsync(RequestUri, content).Result)
                   {
-                      Console.WriteLine(response.Content.ReadAsStringAsync().Result); // Print Out Response
+                      Console.WriteLine(response.Content.ReadAsStringAsync().Result);
                       Console.WriteLine("Batch Sent");
                   }
               }
